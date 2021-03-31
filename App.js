19b0bc2174fdf2498794a401b7f52ee78fd41ce4
chebio <chebio@chebioLCL.cheb.io>
2021-03-31 19:18:59 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import { FirstScreen } from './screens/FirstScreen';
+
+const App= () => {
+  return (
+   <FirstScreen/>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
